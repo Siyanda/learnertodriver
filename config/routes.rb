52f951f423364d0f490f6d9e3752edff388159d9
Siyanda <maphumulo.ps@gmail.com>
2020-12-15 18:12:36 +0200

    rails g devise controlllers, update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#   # sidekiq web interface
+#   require 'sidekiq/web'
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  root 'landing#index'
+
+  devise_for :users,
+  controllers: {
+    sessions: 'users/sessions',
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    unlocks: 'users/unlocks',
+    registrations: 'users/registrations'
+  },
+  :path => '/',
+  :path_names => {
+    :sign_in => 'login',
+    :sign_out => 'logout'
+  }
+
+  resources :users
+#   resources :posts do
+#     resources :comments
+#   end
 end
