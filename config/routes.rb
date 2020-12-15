5bde92eb855e4ba74f7298cf60281353ad9949b7
Siyanda <maphumulo.ps@gmail.com>
2020-12-15 20:07:26 +0200

    add post and comment routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,24 @@
 Rails.application.routes.draw do
-#   # sidekiq web interface
-#   require 'sidekiq/web'
+  # require 'sidekiq/web'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'landing#index'
 
   devise_for :users,
-  controllers: {
-    sessions: 'users/sessions',
-    confirmations: 'users/confirmations',
-    passwords: 'users/passwords',
-    unlocks: 'users/unlocks',
-    registrations: 'users/registrations'
-  },
-  :path => '/',
-  :path_names => {
-    :sign_in => 'login',
-    :sign_out => 'logout'
-  }
+             controllers: {
+               sessions: 'users/sessions',
+               confirmations: 'users/confirmations',
+               passwords: 'users/passwords',
+               unlocks: 'users/unlocks',
+               registrations: 'users/registrations'
+             },
+             path: '/',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout'
+             }
 
   resources :users
-#   resources :posts do
-#     resources :comments
-#   end
+  resources :posts do
+    resources :comments
+  end
 end
