34e6d6957a3b689bbbe6389a524c6193b90c1303
Siyanda <maphumulo.ps@gmail.com>
2020-12-19 05:16:15 +0200

    create seprate layout for registrations pages

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Users::ConfirmationsController < Devise::ConfirmationsController
+  layout "registrations"
+
   # GET /resource/confirmation/new
   # def new
   #   super
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  layout "registrations"
+
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
 
