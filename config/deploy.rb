8c7da9eb517c874b73cfdd96b250f728cfb600a9
Siyanda <maphumulo.ps@gmail.com>
2021-01-20 00:57:39 +0200

    adds server to cap deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-server 'server', port: 8081, roles: [:web, :app, :db], primary: true
+server '45.15.168.229', port: 8081, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'https://31b14cd4c87c4b1f6e002ab6900ab3f27bbe645b@github.com/siyanda/learnertodriver'
 set :application,     'learnertodriver'
