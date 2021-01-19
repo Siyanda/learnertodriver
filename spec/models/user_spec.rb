87f3785d43df932164ffd9bb21c392a7969e5552
Siyanda <maphumulo.ps@gmail.com>
2021-01-20 00:35:00 +0200

    let's try to test abit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(name: "Siyanda Maphumulo",
+                        first_name: "Siyanda",
+                        last_name: "Maphumulo",
+                        username: "maphumulops",
+                        email: "siyanda@mail.com")
+  }
+
+  it "is not valid without a email address" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
 end
