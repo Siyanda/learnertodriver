7ae4b218c7b85997dea848f64491b55a822a76af
Siyanda <maphumulo.ps@gmail.com>
2021-01-20 01:42:12 +0200

    remove deprecated git option, add bundle path, delete unused ngix.conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,10 +23,10 @@ set :puma_worker_timeout, nil
 set :puma_init_active_record, false  # Change to true if using ActiveRecord
 
 ## Defaults:
-set :scm,           :git
 set :branch,        :main
 set :format,        :pretty
 set :log_level,     :debug
+append :linked_dirs, '.bundle'
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
