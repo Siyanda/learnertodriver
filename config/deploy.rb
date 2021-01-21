fcfb8c93c363721572c5d5a83ccf8b81c36170d6
Siyanda <maphumulo.ps@gmail.com>
2021-01-21 07:17:56 +0200

    capistrano for master_key

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ append :linked_dirs, '.bundle'
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/master.key}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
