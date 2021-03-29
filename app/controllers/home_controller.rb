88a9beb34dfd2dbee79d2bf6726a62a978941e1d
Siyanda <maphumulo.ps@gmail.com>
2021-03-30 01:35:11 +0200

    update home view, controller to display landing page with status when no current_user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,16 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @users_count = User.all.size || 0
-    @questions_count = Question.all.size || 0
-    @quizzes_count = Quiz.all.size || 0
+    @users_count = Home.stats[:all_users]
+    @questions_count = Home.stats[:all_questions]
+    @quizzes_count = Home.stats[:all_completed_quizzes]
+
+    return unless current_user
+
+    redirect_to dashboard_path
   end
 
   def show
+    # if user signed in show their stats
   end
 end
