03fed2d291ab04be6970cf5f6a30a17c148802ab
Siyanda <maphumulo.ps@gmail.com>
2021-03-30 02:13:53 +0200

    remove active class helpers, will now use class_names instead

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,31 +14,6 @@ module ApplicationHelper
     url
   end
 
-  def set_active?(key, condition)
-    key.to_s if condition.to_s
-  end
-
-  def contains?(key, param)
-    key.to_s if params.value?(param)
-  end
-
-  # "active" classes for menu links
-  def current?(key, path)
-    "#{key}" if current_page? path
-  end
-
-  def link_to_active(text, destination, options = {})
-    match_text = options.delete(:match_text)
-
-    classes = options[:class].present? ? options[:class].split(" ") : []
-    classes << "active" if request.fullpath.downcase == destination.downcase || (match_text && request.fullpath.downcase.include?(match_text.downcase))
-
-    options = options.except(:class)
-    options.merge!(:class => classes.join(" ")) unless classes.empty?
-
-    link_to(text, destination, options)
-  end
-
   # Pretty Page Title
   def title(page_title)
     content_for(:title) { page_title }
