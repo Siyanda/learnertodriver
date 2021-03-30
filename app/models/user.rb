deed9f96d7433be1991eff14d31e04671da5d86c
Siyanda <maphumulo.ps@gmail.com>
2021-03-30 02:07:06 +0200

    user has_person_name, attached avatar image and enum for status

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,21 @@
 class User < ApplicationRecord
+  has_person_name
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
-  has_many :posts
+  has_many :posts, foreign_key: 'author_id'
   has_many :comments
+  has_many :evaluations
 
   extend FriendlyId
   friendly_id :username, use: :slugged
 
   validates_uniqueness_of :username
+
+  has_one_attached :avatar_image
+
+  enum status: { pending: 0, inactive: 1, active: 2, suspened: 3, blocked: 4 }
+  scope :is_active, -> { where(status: 'active') }
 
   def should_generate_new_friendly_id?
     slug.blank? || username_changed?
@@ -15,7 +23,9 @@ class User < ApplicationRecord
 
   private
 
+  # TODO: check if username exists if true, suggest a similarname
+  # TODO: add Guest user option
   def suggest_username
-    # TODO: check if username exists if true, suggest a similarname
+    # before save ?
   end
 end
