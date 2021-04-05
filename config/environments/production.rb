d55a9fd8dbb4ceb3e123a934122833f29f14d818
Siyanda <maphumulo.ps@gmail.com>
2021-04-05 21:42:05 +0200

    update seeds, add devise mail options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,19 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { :host => ENV["APP_URL"] }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+     :address => ENV["EMAIL_SERVICE_SMTP"],
+     :port => 587,
+     :user_name => ENV["EMAIL_SERVICE_USER"],
+     :password => ENV["EMAIL_SERVICE_PASS"],
+     :authentication => :plain,
+     :enable_starttls_auto => true
+  }
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
