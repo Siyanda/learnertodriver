6bd4ea4342be360269699ca3f60352386a156f24
Siyanda <maphumulo.ps@gmail.com>
2021-04-17 00:28:19 +0200

    run rails update to 6.1 defaults

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_092048) do
+ActiveRecord::Schema.define(version: 2021_04_16_214505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
