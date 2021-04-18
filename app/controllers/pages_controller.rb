d315e5a7bc462309d2b6abba492f6369ec3b77bd
Siyanda <maphumulo.ps@gmail.com>
2021-04-18 21:14:55 +0200

    skip user_auth for new and show on evaluation, scaffold page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,30 +1,70 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_page, only: [:show, :edit, :update, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
-    # index
+    @pages = Page.all
   end
 
   def show
-    # show
+  end
+
+  def new
+    @page = current_user.pages.build
   end
 
   def edit
-    # show
+  end
+
+  def create
+    @page = current_user.pages.build(page_params)
+
+    respond_to do |format|
+      if @page.save
+        format.html { redirect_to @page, notice: 'Page was successfully created.' }
+        format.json { render :show, status: :created, location: @page }
+      else
+        format.html { render :new }
+        format.json { render json: @page.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
-    # show
+    respond_to do |format|
+      if @page.update(page_params)
+        format.html { redirect_to @page, notice: 'Page was successfully updated.' }
+        format.json { render :show, status: :ok, location: @page }
+      else
+        format.html { render :edit }
+        format.json { render json: @page.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @page.destroy
+    respond_to do |format|
+      format.html { redirect_to pages_path, notice: 'Page was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
+
+  def require_same_user
+    if current_user != @page.author
+      flash[:danger] = "Not authorized to edit this page"
+      redirect_to root_path
+    end
+  end
 
   def set_page
     @page = Page.friendly.find(params[:id])
   end
 
   def page_params
-    params.require(:page).permit(:title, :template)
+    params.require(:page).permit( :title, :template, :author_id)
   end
 end
