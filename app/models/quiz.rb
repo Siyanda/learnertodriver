3ae990ec9f50ccb2a608ddec296e4ca47ef9350a
Siyanda <maphumulo.ps@gmail.com>
2021-04-18 21:02:24 +0200

    optional cover image for quizzes

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -7,5 +7,7 @@ class Quiz < ApplicationRecord
   has_many :specifications
   has_many :questions, through: :specifications
 
+  has_one_attached :cover_image
+
   enum kind: { general: 0, truck: 1, car: 2, bike: 3 }
 end
