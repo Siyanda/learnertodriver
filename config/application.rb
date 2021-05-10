f4383e630070f750543fae81fdee0608b65ffe7e
Siyanda <maphumulo.ps@gmail.com>
2021-05-11 01:24:00 +0200

    set timezone to Pretoria

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Learnertodriver
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Pretoria"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
