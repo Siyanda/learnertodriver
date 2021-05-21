b9517ca40a24bac6ccc61708c8c2a29052921fc9
Siyanda <maphumulo.ps@gmail.com>
2021-05-21 13:05:52 +0200

    allow sign up with username, fixes not null error on sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[username first_name last_name])
   end
 end
