768d1bf3d7b70799d1df2379f6b216d37d59df01
Siyanda <maphumulo.ps@gmail.com>
2021-07-02 02:59:50 +0200

    update views, show tags

--- app/assets/javascripts/libraries/main.js
+++ app/assets/javascripts/libraries/main.js
@@ -12,5 +12,17 @@ document.addEventListener("turbo:load", function() {
         element.innerHTML = formatedDate;
     });
   }
-})
-;
+
+  let timeContainer = document.getElementsByClassName("formatted_time")
+
+  if (timeContainer.length > 0) {
+    let allDates = Array.from(timeContainer);
+
+      allDates.forEach(function(element) {
+        let dateText = element.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim();
+        let formatedDate = dayjs(dateText).format('D MMM YYYY');
+        element.innerHTML = formatedDate;
+    });
+  }
+});
+
--- config/routes.rb
+++ config/routes.rb
@@ -37,8 +37,7 @@ Rails.application.routes.draw do
 
   resources :posts do
     resources :comments
-    resources :tags, module: :posts
-    member do
+     member do
       put 'like', to:    'posts#upvote'
       put 'dislike', to: 'posts#downvote'
     end
