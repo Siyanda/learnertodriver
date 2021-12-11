902bea1a8efb4cf148671e0c7b1b2cbd2c35bd78
Siyanda <maphumulo.ps@gmail.com>
2021-12-11 03:54:08 +0200

    create question and slide controller

--- app/javascript/controllers/slides_controller.js
+++ app/javascript/controllers/slides_controller.js
@@ -3,14 +3,49 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   static get targets() {
-    return ['logo','icon','wrapper','bar']
+    return ['slide']
   }
 
-  toggle(event) {
-    this.iconTarget.classList.toggle('hidden');
-    this.logoTarget.classList.toggle('hidden');
+  connect(){
 
-    this.wrapperTarget.classList.toggle('right-side');
-    this.barTarget.classList.toggle('hidden');
   }
+
+  next(){
+    if(this.indexValue < (this.slideTargets.length - 1)){
+      this.indexValue++;
+    }else{
+      this.indexValue = 0;
+    }
+  }
+
+  previous(){
+    if(this.indexValue > 0){
+      this.indexValue--;
+    }else{
+      this.indexValue = this.slideTargets.length - 1;
+    }
+   }
+
+  stopSlideShow(){
+    if(this.slideShowTimer){
+      clearInterval(this.slideShowTimer);
+    }
+  }
+
+  disconnect(){
+    this.stopSlideShow();
+  }
+
+  showCurrentSlide() {
+    this.slideTargets.forEach((element, index) => {
+      //element.hidden = index != this.indexValue;
+      var isHidden = index != this.indexValue;
+      if(isHidden){
+        element.classList.remove('active');
+      }else{
+        element.classList.add('active');
+      }
+    })
+   }
+
 }
