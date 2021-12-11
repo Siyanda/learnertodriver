25277146a132c9fab313b349d4edc856e70a4c65
Siyanda <maphumulo.ps@gmail.com>
2021-12-11 03:02:09 +0200

    fix method redirect

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @post, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @comment.post, notice: 'Comment was successfully created.' }
         format.json { render json: @comment, status: :created, location: @comment }
       else
         format.html { render action: 'new' }
@@ -22,6 +22,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
+      format.html { redirect_to @comment.post, notice: 'Comment was successfully deleted.', status: :see_other }
       format.json { head :no_content }
     end
   end
---
+++ app/javascript/controllers/slides_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+
+  static get targets() {
+    return ['logo','icon','wrapper','bar']
+  }
+
+  toggle(event) {
+    this.iconTarget.classList.toggle('hidden');
+    this.logoTarget.classList.toggle('hidden');
+
+    this.wrapperTarget.classList.toggle('right-side');
+    this.barTarget.classList.toggle('hidden');
+  }
+}
