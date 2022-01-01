916fd44a081a5313bf3def123bfb231b1dfb5f89
Siyanda <maphumulo.ps@gmail.com>
2022-01-02 00:42:49 +0200

    use single quotes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,10 @@
-import { Turbo } from "@hotwired/turbo-rails"
+import { Turbo } from '@hotwired/turbo-rails'
 
-import "trix"
-import "@rails/actiontext"
-import "./controllers"
-import "./channels"
-import "./vendor/"
+import 'trix'
+import '@rails/actiontext'
+import './controllers'
+import './channels'
+import './vendor/'
 
 // Make accessible for Electron and Mobile adapters
 window.Turbo = Turbo
