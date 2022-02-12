2066726841b509e522c5c611f9fce1b31d3ae8f9
Siyanda Maphumulo <maphumulo.ps@gmail.com>
2022-02-12 02:32:43 +0200

    turbo updates

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,6 @@
 require_relative "boot"
-
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Learnertodriver
@@ -13,6 +10,5 @@ module Learnertodriver
 
     config.time_zone = "Africa/Johannesburg"
     config.active_job.queue_adapter = :sidekiq
-    # config.eager_load_paths << Rails.root.join("extras")  end
   end
 end
