f2018b3a6ba6d858f6618d9e582e5fc68c3f0760
Maphumulo <maphumulo.ps@gmail.com>
2022-03-26 19:41:15 +0200

    Update config (#28)
    * update ignore
    
    * update config
    
    * update packages
    
    * update styles
    
    * use credentials
    
    * update views
    
    * dev updates
    
    * set quiz state

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -46,7 +46,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
@@ -90,4 +90,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: ENV['APP_URL'] }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address: Rails.application.credentials.send_grid.smtp,
+    port: Rails.application.credentials.send_grid.port,
+    user_name: Rails.application.credentials.send_grid.user,
+    password: Rails.application.credentials.send_grid.password,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
 end
