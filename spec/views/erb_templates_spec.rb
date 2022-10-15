d30a3879bf15bc07709e175945f2898a93cb0abe
Siyanda Maphumulo <maphumulo.ps@gmail.com>
2022-10-15 22:56:10 +0200

    linting

--- spec/views/erb_templates_spec.rb
+++ spec/views/erb_templates_spec.rb
@@ -2,11 +2,8 @@
 
 require 'better_html'
 
-# Based on https://github.com/Shopify/better-html#testing-for-valid-html-and-erb
 RSpec.describe 'ERB templates' do # rubocop:disable RSpec/DescribeClass
-  Dir[
-    Rails.root.join('app/views/**/{*.htm,*.html,*.htm.erb,*.html.erb,*.html+*.erb}')
-  ].each do |filename|
+  Dir[Rails.root.join('app/views/**/{*.htm,*.html,*.htm.erb,*.html.erb,*.html+*.erb}')].each do |filename|
     describe Pathname.new(filename).relative_path_from(Rails.root) do
       it 'is valid' do
         expect(BetterHtml::BetterErb::ErubiImplementation.new(File.read(filename)).validate!).to be_nil
