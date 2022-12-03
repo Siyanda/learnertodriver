0bf69fe32d78ca873d9d012de7309829ae7ca866
Siyanda <maphumulo.ps@gmail.com>
2022-12-04 00:26:34 +0200

    update new quiz form

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class EvaluationsController < ApplicationController
-  # skip_before_action :authenticate_user!, only: %i[show new]
   before_action :set_evaluation, only: %i[show edit update destroy]
   before_action :set_user, only: %i[show edit create update destroy]
 
