b5fcc1997e1d56f960e7234826c9740490c38ade
Siyanda <maphumulo.ps@gmail.com>
2023-01-03 01:31:38 +0200

    update helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start 'rails' do
+  minimum_coverage 2
+end
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -14,9 +21,51 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 
+Capybara.server = :puma, { Silent: true }
+
+Capybara.register_driver :firefox do |app|
+  Capybara::Selenium::Driver.new(app, browser: :firefox)
+end
+
+Capybara.register_driver :headless_firefox do |app|
+  caps = Selenium::WebDriver::Remote::Capabilities.firefox('goog:loggingPrefs': { browser: 'ALL' })
+  opts = Selenium::WebDriver::Firefox::Options.new
+
+  firefox_args = %w[--headless --no-sandbox --disable-gpu --window-size=1920,1080 --remote-debugging-port=9222]
+  firefox_args.each { |arg| opts.add_argument(arg) }
+  Capybara::Selenium::Driver.new(app, browser: :firefox, capabilities: [caps, opts])
+end
+
+Capybara.javascript_driver =
+  case ENV['HEADLESS']
+  when 'true', 1, nil
+    :headless_firefox
+  else
+    :firefox
+  end
+
+Capybara.enable_aria_label = true
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 RSpec.configure do |config|
-  config.fixture_path = "#{Rails.root}/spec/fixtures"
+  config.before(:each, type: :system) do
+    driven_by Capybara.javascript_driver
+  end
+
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
+
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+
+  config.include Devise::Test::IntegrationHelpers, type: :system
+  config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Warden::Test::Helpers
+  config.include FactoryBot::Syntax::Methods
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,4 +10,6 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.order = :random
+  config.disable_monkey_patching!
 end
