bc8c19692aa00d37ef00df8b49eb6486f4d90acf
Siyanda <maphumulo.ps@gmail.com>
2023-01-03 19:11:40 +0200

    update configs

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
-require_relative "boot"
-require "rails/all"
+require_relative 'boot'
+require 'rails'
+# Pick the frameworks you want:
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+# require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
+# require 'action_cable/engine'
+# require 'rails/test_unit/railtie'
 
 Bundler.require(*Rails.groups)
 
@@ -9,8 +21,8 @@ module Learnertodriver
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    config.time_zone = "Africa/Johannesburg"
+    config.time_zone = 'Pretoria'
     config.active_job.queue_adapter = :sidekiq
+    config.generators.system_tests = nil
   end
 end
