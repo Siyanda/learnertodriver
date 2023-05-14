ddd0e8ae13160fafd9fc61481a25dfcb97fd1d34
Maphumulo <maphumulo.ps@gmail.com>
2023-05-14 21:24:34 +0200

    chore: enable rubocop StringLiterals

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
-require_relative "boot"
-require "rails/all"
+require_relative 'boot'
+require 'rails/all'
 
 Bundler.require(*Rails.groups)
 
 module Learnertodriver
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.time_zone = "Africa/Johannesburg"
+    config.time_zone = 'Africa/Johannesburg'
     config.active_job.queue_adapter = :litejob
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Specify AnyCable WebSocket server URL to use by JS client
   config.after_initialize do
-    config.action_cable.url = ActionCable.server.config.url = ENV.fetch("CABLE_URL", "/cable") if AnyCable::Rails.enabled?
+    config.action_cable.url = ActionCable.server.config.url = ENV.fetch('CABLE_URL', '/cable') if AnyCable::Rails.enabled?
   end
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -88,7 +88,7 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
+  if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
