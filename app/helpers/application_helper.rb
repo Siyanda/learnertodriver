4c7e4b8424f6b40120c6111cd0b90d067fc6dd71
Maphumulo <maphumulo.ps@gmail.com>
2024-11-11 20:08:34 +0200

    feat: use meta tag helper (#97)
    * feat: use meta tag helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,27 @@
 module ApplicationHelper
   include Pagy::Frontend
 
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
+  def url_contains?(klass, path)
+    klass if request.path.include?(path.to_s)
+  end
+
+  def inline_svg(path)
+    File.open("app/assets/images/svg/#{path}", 'rb') do |file|
+      raw file.read
+    end
+  end
+
   def from_markdown(text)
     options = %i[autolink fenced_code_blocks highlight no_intra_emphasis]
     Markdown.new(text, *options).to_html
+  end
+
+  def meta_tag(name, content)
+    tag.meta(name: , content: ) unless content.nil?
   end
 
   def liquidize(content)
@@ -23,21 +41,5 @@ module ApplicationHelper
       url += "#{key}=#{value}"
     end
     url
-  end
-
-  # Pretty Page Title
-  def title(page_title)
-    content_for(:title) { page_title }
-  end
-
-  def url_contains?(klass, path)
-    klass if request.path.include?(path.to_s)
-  end
-
-  # inline SVG
-  def inline_svg(path)
-    File.open("app/assets/images/svg/#{path}", 'rb') do |file|
-      raw file.read
-    end
   end
 end
