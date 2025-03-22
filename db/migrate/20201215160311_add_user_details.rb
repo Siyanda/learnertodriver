b81f51a03203c9932ebbcd4cadca38b5af8300a1
Maphumulo <maphumulo.ps@gmail.com>
2025-03-22 17:52:33 +0200

    chore: solid cable cache and queue schema updates

--- db/migrate/20201215160311_add_user_details.rb
+++ db/migrate/20201215160311_add_user_details.rb
@@ -3,18 +3,18 @@
 class AddUserDetails < ActiveRecord::Migration[6.1]
   def change
     change_table :users, bulk: true do |t|
-      t.string :first_name
-      t.string :last_name
-      t.text :bio
-      t.text :links
-      t.date :birthday
-      t.integer :role, default: 0, null: false
-      t.integer :status, default: 0, null: false
-      t.string :username
-      t.integer :evaluations_count, default: 0, null: false
-      t.string :slug
+      t.string   :first_name
+      t.string   :last_name
+      t.text     :bio
+      t.text     :links
+      t.date     :birthday
+      t.string   :username
+      t.integer  :role,              default: 0, null: false
+      t.integer  :status,            default: 0, null: false
+      t.integer  :evaluations_count, default: 0, null: false
+      t.string   :slug
     end
-    add_index :users, :slug, unique: true
+    add_index :users, :slug,     unique: true
     add_index :users, :username, unique: true
   end
 end
