98a8d380ca1be085ea9062beb6286e8d011fcf8d
Maphumulo <maphumulo.ps@gmail.com>
2025-04-05 00:19:39 +0200

    fix: fixes issues with views

--- config/routes.rb
+++ config/routes.rb
@@ -22,10 +22,6 @@ Rails.application.routes.draw do
 
   resources :posts do
     resources :comments
-    member do
-      put 'like',    to: 'posts#upvote'
-      put 'dislike', to: 'posts#downvote'
-    end
   end
 
   get '/manifest.json',            to: 'service_worker#manifest'
