dbe176c9295a2e046e5165e4bef513df6fb89f81
Maphumulo <maphumulo.ps@gmail.com>
2025-04-05 02:51:34 +0200

    test: setup system tests

---
+++ test/test_helpers/system/better_rails_system_tests.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module BetterRailsSystemTests
+  def absolute_image_path
+    Rails.root.join("#{Capybara.save_path}/screenshots/#{image_name}.png")
+  end
+
+  def take_screenshot
+    return super unless Capybara.last_used_session
+
+    Capybara.using_session(Capybara.last_used_session) { super }
+  end
+end
