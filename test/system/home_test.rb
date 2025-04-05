3d97c6eb2f445a68887194ce83d1973ed0b05401
Maphumulo <maphumulo.ps@gmail.com>
2025-04-05 02:52:13 +0200

    test: setup system tests

--- test/system/home_test.rb
+++ test/system/home_test.rb
@@ -3,7 +3,7 @@
 require 'application_system_test_case'
 
 class HomeTest < ApplicationSystemTestCase
-  test 'visiting the index' do
+  test 'visiting the root page' do
     visit root_url
 
     assert_selector 'h1', text: /#{I18n.t('site_name')}/i
