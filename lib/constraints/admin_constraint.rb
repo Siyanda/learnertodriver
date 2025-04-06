9f564d7baabdcec28655238fb0198bc85453f55e
Maphumulo <maphumulo.ps@gmail.com>
2025-04-06 14:02:50 +0200

    feat: namespace admin route constraint

--- lib/constraints/admin_constraint.rb
+++ lib/constraints/admin_constraint.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-class AdminConstraint
-  def matches?(request)
-    user_id = request.session[:user_id]
-    return false unless user_id
+module Constraints
+  class AdminConstraint
+    def matches?(request)
+      user_id = request.session[:user_id]
+      return false unless user_id
 
-    user = User.find_by(id: user_id)
-    user&.admin?
+      user = User.find_by(id: user_id)
+      user&.admin?
+    end
   end
 end
