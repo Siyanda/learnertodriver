d2b2333a089c177afc922a0a0dfc26976b6239f1
Maphumulo <maphumulo.ps@gmail.com>
2025-04-06 13:51:46 +0200

    feat: restrict admin routes

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,6 @@ module Learnertodriver
   class Application < Rails::Application
     config.load_defaults 7.0
     config.time_zone = 'Africa/Johannesburg'
+    config.autoload_lib(ignore: %w[assets tasks])
   end
 end
