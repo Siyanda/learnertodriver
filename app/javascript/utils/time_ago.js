3cfe921d759b427305223de91d9067004ff5e8da
Maphumulo <maphumulo.ps@gmail.com>
2025-04-18 15:03:07 +0200

    feat: Bun tests (#129)
    * feat: update test workflow to cache bun install
    * feat: add bun test

---
+++ app/javascript/utils/time_ago.js
@@ -0,0 +1,20 @@
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+dayjs.extend(relativeTime)
+
+export function getRelativeTimeString(isoTime) {
+  return dayjs(isoTime).fromNow()
+}
+
+export function getNextInterval(ageInSeconds) {
+  if (ageInSeconds < 60) {
+    return 1000
+  } else if (ageInSeconds < 3600) {
+    return 60 * 1000
+  } else if (ageInSeconds < 86400) {
+    return 60 * 60 * 1000
+  } else {
+    return null
+  }
+}
