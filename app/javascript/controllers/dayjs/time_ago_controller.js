2a37bef3bdfd57a686d8c3ec6f68307a931b8a43
Maphumulo <maphumulo.ps@gmail.com>
2025-04-18 17:02:42 +0200

    feat: use interval util for time_ago controller

--- app/javascript/controllers/dayjs/time_ago_controller.js
+++ app/javascript/controllers/dayjs/time_ago_controller.js
@@ -1,39 +1,34 @@
 import { Controller } from '@hotwired/stimulus'
+import { getRelativeTimeString, getNextInterval } from '../../utils/time_ago.js'
 import dayjs from 'dayjs'
 
-const relativeTime = require('dayjs/plugin/relativeTime');
-dayjs.extend(relativeTime);
-
 export default class extends Controller {
+  static targets = ['formattedTime']
+  static values = { timeStamp: String }
 
-  static get targets() {
-    return ['formattedTime']
-  }
-
-  static values = {
-    timeStamp: String
-  }
-
-  initialize() {
+  connect() {
     this.updateText()
-    this.autoSlide()
+    this.scheduleNextUpdate()
   }
 
-  autoSlide() {
-    this.interval = setInterval(() => { console.log('1 min')}, 60000)
-
-    this.timeout = setTimeout(() => {
-      console.log("end")
-    }, 60000)
+  disconnect() {
+    if (this.timeout) clearTimeout(this.timeout)
   }
 
   updateText() {
-    this.formattedTimeTarget.innerText = dayjs(this.timeStampValue).fromNow()
+    const formatted = getRelativeTimeString(this.timeStampValue)
+    this.formattedTimeTarget.innerText = formatted
   }
 
-  atInterval(task,interval,end) {
-    setTimeout(() => { clearInterval(timerId); alert('stop'); }, 5000);
-    let timerId = setInterval(() => alert('tick'), 2000);
-    // set timeout to update time every minute for 60 minutes, then every hour for 24 hours, then daily
+  scheduleNextUpdate() {
+    const ageInSeconds = dayjs().diff(dayjs(this.timeStampValue), 'second')
+    const delay = getNextInterval(ageInSeconds)
+
+    if (delay !== null) {
+      this.timeout = setTimeout(() => {
+        this.updateText()
+        this.scheduleNextUpdate()
+      }, delay)
+    }
   }
 }
