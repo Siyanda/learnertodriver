46695d8fd2e732e6bd505d9b175e37ddcc1e64b9
Siyanda Maphumulo <maphumulo.ps@gmail.com>
2025-08-11 00:32:22 +0200

    feat: add action policies

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -6,4 +6,8 @@ class ApplicationPolicy < ActionPolicy::Base
   def owner?
     record.user_id == user.id
   end
+
+  def admin?
+    user.admin?
+  end
 end
--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -1,15 +1,7 @@
 # frozen_string_literal: true
 
 class CommentPolicy < ApplicationPolicy
-  def show?
-    true
-  end
-
-  def update?
-    user.id == record.user_id
-  end
-
   def destroy?
-    user.id == record.user_id
+    owner?
   end
 end
