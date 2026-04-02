f40dd10f3be7ed473008f8ff781fd1840d5ee50a
Siyanda Maphumulo <maphumulo.ps@gmail.com>
2026-04-03 01:51:14 +0200

    test: add test for actions

--- test/actions/evaluations/find_user_started_evaluation_test.rb
+++ test/actions/evaluations/find_user_started_evaluation_test.rb
@@ -3,4 +3,43 @@
 require 'test_helper'
 
 class Evaluations::FindUserStartedEvaluationTest < ActiveSupport::TestCase
+  setup do
+    @user = create(:user)
+    @quiz = create(:quiz)
+  end
+
+  test 'finds an started evaluation for the user and quiz' do
+    evaluation = create(:evaluation, user: @user, quiz: @quiz, status: :started)
+    ctx = execute
+
+    assert_equal evaluation, ctx.evaluation
+  end
+
+  test 'returns nil when the user has no started evaluation for the quiz' do
+    create(:evaluation, user: @user, quiz: @quiz, status: :completed)
+
+    assert_nil execute.evaluation
+  end
+
+  test 'does not return an started evaluation belonging to a different user' do
+    create(:evaluation, user: create(:user), quiz: @quiz, status: :started)
+
+    assert_nil execute.evaluation
+  end
+
+  test 'does not return an started evaluation for a different quiz' do
+    create(:evaluation, user: @user, quiz: create(:quiz), status: :started)
+
+    assert_nil execute.evaluation
+  end
+
+  test 'action succeeds regardless of whether an evaluation is found' do
+    assert_predicate execute, :success?
+  end
+
+  private
+
+  def execute
+    Evaluations::FindUserStartedEvaluation.execute(user: @user, quiz: @quiz)
+  end
 end
--- test/organizers/evaluations/calculate_score_test.rb
+++ test/organizers/evaluations/calculate_score_test.rb
@@ -4,17 +4,18 @@ require 'test_helper'
 
 class Evaluations::CalculateScoreTest < ActiveSupport::TestCase
   test 'calculates scores for all choices in the evaluation' do
-    quiz     = create(:quiz)
-    question = create(:question)
-    correct  = create(:answer, question:)
-    wrong    = create(:answer, question:)
-    create(:correct_answer, question:, answer: correct)
+    quiz      = create(:quiz)
+    question  = create(:question)
+    correct   = create(:answer, question:)
+    incorrect = create(:answer, question:)
 
     quiz.questions << question
 
+    create(:correct_answer, question:, answer: correct)
+
     evaluation    = create(:evaluation, user: create(:user), quiz:)
     first_choice  = create(:choice, evaluation:, question:, answer: correct)
-    second_choice = create(:choice, evaluation:, question:, answer: wrong)
+    second_choice = create(:choice, evaluation:, question:, answer: incorrect)
 
     Evaluations::CalculateScore.call(evaluation)
 
