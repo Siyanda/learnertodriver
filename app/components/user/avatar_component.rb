bfb9e3ea1ad9bd0df596629fad0587d12e3a1f32
Siyanda Maphumulo <maphumulo.ps@gmail.com>
2026-09-09 23:28:59 +0200

    feat: updates

--- app/components/user/avatar_component.rb
+++ app/components/user/avatar_component.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 
-class User::AvatarComponent < ViewComponent::Base
+class User::AvatarComponent < ApplicationViewComponent
+  option :user,       required: true
+  option :size,       default: -> { 80 }
+  option :html_class, default: -> { 'avatar' }
+
+  def image_url
+    if user.avatar.attached?
+      url_for(user.avatar.variant(resize_to_limit: [size, size]))
+    else
+      gravatar_url
+    end
+  end
+
+  private
+
+  def gravatar_url
+    email = user.email_address.to_s.strip.downcase
+    hash = Digest::MD5.hexdigest(email)
+    "https://www.gravatar.com/avatar/#{hash}?s=#{size}&d=identicon"
+  end
 end
--- test/components/user/avatar_component_test.rb
+++ test/components/user/avatar_component_test.rb
@@ -3,10 +3,30 @@
 require 'test_helper'
 
 class User::AvatarComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(User::AvatarComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  include ActionDispatch::TestProcess::FixtureFile
+
+  def test_renders_gravatar_when_no_avatar_attached
+    user = create(:user, email_address: 'sadio@example.com')
+
+    component = render_inline(User::AvatarComponent.new(user: user))
+
+    hash = Digest::MD5.hexdigest('sadio@example.com')
+    assert_equal "https://www.gravatar.com/avatar/#{hash}?s=80&d=identicon", component.css('img').first['src']
+  end
+
+  def test_renders_custom_size_in_gravatar_url
+    user = create(:user, email_address: 'sadio@example.com')
+
+    component = render_inline(User::AvatarComponent.new(user: user, size: 200))
+
+    assert_includes component.css('img').first['src'], 's=200'
+  end
+
+  def test_renders_custom_html_class
+    user = create(:user)
+
+    component = render_inline(User::AvatarComponent.new(user: user, html_class: 'rounded shadowed'))
+
+    assert_equal 'rounded shadowed', component.css('img').first['class']
   end
 end
