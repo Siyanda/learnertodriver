a46121487adc13636bb454f33154d9c9388ce509
Siyanda Maphumulo <maphumulo.ps@gmail.com>
2026-09-09 17:30:37 +0200

    test: update test config

--- test/components/utils/inline_svg_component_test.rb
+++ test/components/utils/inline_svg_component_test.rb
@@ -3,10 +3,24 @@
 require 'test_helper'
 
 class Utils::InlineSvgComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(Utils::InlineSvgComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  def test_renders_svg_content
+    svg = render_inline(Utils::InlineSvg::Component.new(path: 'icons/address.svg'))
+
+    assert_includes svg.to_html, '<svg'
+    assert_includes svg.to_html, 'xmlns="http://www.w3.org/2000/svg"'
+  end
+
+  def test_raises_when_svg_not_found
+    error = assert_raises(ArgumentError) do
+      render_inline(Utils::InlineSvg::Component.new(path: 'missing.svg'))
+    end
+
+    assert_equal 'SVG not found: missing.svg', error.message
+  end
+
+  def test_raises_when_path_escapes_svg_directory
+    assert_raises(ArgumentError) do
+      render_inline(Utils::InlineSvg::Component.new(path: '../../config/database.yml'))
+    end
   end
 end
