4ecd44e12be05b52e1f706eb630fbb7c849e0b3b
Siyanda Maphumulo <maphumulo.ps@gmail.com>
2026-09-11 20:44:59 +0200

    test: components tests

---
+++ test/frontend/components/quiz/preview_card_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Quiz::PreviewCard::ComponentTest < ViewComponent::TestCase
+  def test_renders
+    component = build_component
+
+    render_inline(component)
+
+    assert_selector "div"
+  end
+
+  private
+
+  def build_component(**options)
+    Quiz::PreviewCard::Component.new(**options)
+  end
+end
